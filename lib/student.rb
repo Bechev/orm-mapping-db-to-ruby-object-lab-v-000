3d7e95ac5b5fcb4d1abbf59016672614f8b56776
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-03-09 03:32:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -59,6 +59,18 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?;
+    SQL
+
+    DB[:conn].execute(sql,x).map do |row|
+     self.new_from_db(row)
+    end
+  end
+
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
@@ -66,7 +78,7 @@ class Student
       WHERE grade = 10
       ORDER BY students.id LIMIT 1;
     SQL
-
+  
     DB[:conn].execute(sql).map do |row|
      self.new_from_db(row)
    end.first
